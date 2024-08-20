cefe685ac941ec466a8c17f5b35e4fec108deef2
edoj2k <edwardferdian@yahoo.com>
2024-08-20 16:54:28 +0700

    49-events part 3

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-// Lecture: Get And Manipulate On Individual Elements - Part 1
+// let div = document.querySelector('div.wrapper');
+// let header = document.querySelector('header');
+// let h2 = document.querySelector('header h2');
 
-/*
-console.log(document.getElementById('list'));
+// div.addEventListener('click', function(){
+//     let delay = new Date().getTime() + 1000;
+//     while(new Date() < delay) {}
+//     console.log('From div');
+// }, true);
 
-var el = document.getElementById('list');
-console.log(el);
-console.log(typeof el);
+// header.addEventListener('click', function(){
+//     let delay = new Date().getTime() + 1000;
+//     while(new Date() < delay) {}
+//     console.log('From header');
+// }, true);
 
-// var el = document.getElementById('LIST');
-// console.log(el);
-
-console.log(el.textContent);
-console.log(el.textContent = 'Hello');
-
-var heading = document.getElementById('heading').textContent;
-console.log(heading);
-
-var ulElement = document.getElementById('list');
-console.log(ulElement.textContent = heading);
-
-console.log(el.innerHTML);
-console.log(el.innerHTML = 'Hello');
-console.log(el.innerHTML = '<h1>Hello</h1>');
-console.log(el.textContent = '<h1>Hello</h1>');
-*/
-
-// let lis = document.querySelectorAll('li, h2, #hide-list');
-
-// console.log(lis);
-
-// Array.from(lis).forEach(function(li) {
-//     li.textContent = 'hello world';
-// })
+// h2.addEventListener('click', function(){
+//     let delay = new Date().getTime() + 1000;
+//     while(new Date() < delay) {}
+//     console.log('From h2');
+// }, true);
 
 let h2 = document.querySelector('header h2');
 
-h2.addEventListener('click', a);
+h2.addEventListener('click', function() {
+    console.log('From click event');
+});
 
 function a() {
-    console.log(this);
+    let delay = new Date().getTime() + 3000;
+    while(new Date() < delay) {}
+    console.log('From function a');
 }
+a();
