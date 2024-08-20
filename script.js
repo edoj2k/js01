587283d972117c8db8f5f25a557c3470c5fa49e4
edoj2k <edwardferdian@yahoo.com>
2024-08-20 16:30:54 +0700

    48-events part2

--- script.js
+++ script.js
@@ -35,12 +35,8 @@ console.log(el.textContent = '<h1>Hello</h1>');
 
 let h2 = document.querySelector('header h2');
 
-h2.className = 'changeBg';
+h2.addEventListener('click', a);
 
-h2.classList.add('changeFt');
-
-// h2.classList.remove('changeBg');
-
-h2.classList.toggle('changeBg');
-
-console.log(h2.classList);
+function a() {
+    console.log(this);
+}
