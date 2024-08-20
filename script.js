7c1cd1b9c7818f8ff0f2b93580ffdd33961c4bc2
edoj2k <edwardferdian@yahoo.com>
2024-08-20 11:56:07 +0700

    46-dom styles and classes

--- script.js
+++ script.js
@@ -33,5 +33,14 @@ console.log(el.textContent = '<h1>Hello</h1>');
 //     li.textContent = 'hello world';
 // })
 
+let h2 = document.querySelector('header h2');
 
+h2.className = 'changeBg';
 
+h2.classList.add('changeFt');
+
+// h2.classList.remove('changeBg');
+
+h2.classList.toggle('changeBg');
+
+console.log(h2.classList);
