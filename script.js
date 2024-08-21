3fa06e346024ca5a2683185da4ec14677a39c408
edoj2k <edwardferdian@yahoo.com>
2024-08-21 15:50:08 +0700

    63-function constructor prototypes

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let a = {};
-let b = new Object();
+function Person(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    // this.getFullName = function() {
+    //     return this.firstname + ' ' + this.lastname;
+    // }
+}
 
-console.log(a);
-console.log(b);
+Person.prototype.getFullName = function() {
+    return this.firstname + ' ' + this.lastname;
+}
 
-Object.prototype.greet = 'hello';
+Person.prototype.greet = 'Hello from greet';
+
+let person1 = new Person('John', 'Smith');
+person1.greet = 'hello from person1 object';
+
+let person2 = new Person('Nick', 'Doe');
+
+console.log(person1.getFullName());
+console.log(person2.getFullName());
