57dab16bb4265d719c808b2b162020beff7d416b
edoj2k <edwardferdian@yahoo.com>
2024-08-21 15:16:51 +0700

    62-prototype object function

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-let person1 = {};
-person1.firstname = 'John';
-person1.lastname = 'Smith';
+let a = {};
+let b = new Object();
 
-let person2 = {};
-person2.firstname = 'Nick';
-person2.lastname = 'Doe';
+console.log(a);
+console.log(b);
 
-// function createPerson(firstname, lastname) {
-//     let newPerson = {};
-//     newPerson.firstname = firstname;
-//     newPerson.lastname = lastname;
-//     return newPerson
-// }
-
-function Person(firstname, lastname) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-}
-
-let person3 = new Person('Bob', 'Brown');
-let person4 = new Person('Marry', 'James');
-
-console.log(person1);
-console.log(person2);
-console.log(person3);
-console.log(person4);
-
-let strObj = new String('hello');
-console.log(strObj);
-console.log(typeof strObj);
+Object.prototype.greet = 'hello';
