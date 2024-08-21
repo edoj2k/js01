9dddee63d38f4a7fdd1cd65fc599f5e64882001c
edoj2k <edwardferdian@yahoo.com>
2024-08-21 15:07:42 +0700

    61-function constructor

--- script.js
+++ script.js
@@ -1,99 +1,31 @@
-let ul = document.querySelector('ul');
+let person1 = {};
+person1.firstname = 'John';
+person1.lastname = 'Smith';
 
-// ******** add item
-document.getElementById('add-btn').addEventListener('click',
-    function(e) {
-        e.preventDefault();
+let person2 = {};
+person2.firstname = 'Nick';
+person2.lastname = 'Doe';
 
-        let addInput = document.getElementById('add-input');
+// function createPerson(firstname, lastname) {
+//     let newPerson = {};
+//     newPerson.firstname = firstname;
+//     newPerson.lastname = lastname;
+//     return newPerson
+// }
 
-        if(addInput.value !== '') {
-            let li = document.createElement('li'),
-                firstPar = document.createElement('p'),
-                secondPar = document.createElement('p'),
-                firstIcon = document.createElement('i'),
-                secondIcon = document.createElement('i'),
-                input = document.createElement('input');
-    
-            firstIcon.className = "fa fa-pencil-square-o";
-            secondIcon.className = "fa fa-times";
-            input.className = "edit-note";
-            input.setAttribute('type', 'text');
-    
-            firstPar.textContent = addInput.value;
-    
-            secondPar.appendChild(firstIcon);
-            secondPar.appendChild(secondIcon);
-            li.appendChild(firstPar);
-            li.appendChild(secondPar);
-            li.appendChild(input);
-            ul.appendChild(li);
-            addInput.value = '';
-        };
-    }
-);
+function Person(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
 
-// ********* edit and delete items
-ul.addEventListener('click', function(e) {
-    if(e.target.classList[1] === "fa-pencil-square-o") {
-        let parentPar = e.target.parentNode;
-        parentPar.style.display = 'none';
+let person3 = new Person('Bob', 'Brown');
+let person4 = new Person('Marry', 'James');
 
-        let note = parentPar.previousElementSibling;
-        let input = parentPar.nextElementSibling;
+console.log(person1);
+console.log(person2);
+console.log(person3);
+console.log(person4);
 
-        input.style.display = 'block';
-        input.value = note.textContent;
-
-        input.addEventListener('keypress', function(e) {
-            if(e.keyCode === 13) {
-                if(input.value !== '') {
-                    note.textContent = input.value;
-                    parentPar.style.display = 'block';
-                    input.style.display = 'none';
-                } else {
-                    let li = input.parentNode;
-                    console.log(li);
-                    li.parentNode.removeChild(li);
-                }
-            } 
-        }, false);
-    } else if(e.target.classList[1] === "fa-times") {
-        let list = e.target.parentNode.parentNode;
-        list.parentNode.removeChild(list);
-    }
-});
-
-// ********* hide items
-let hideItem = document.getElementById('hide');
-
-hideItem.addEventListener('click', function() {
-    let label = document.querySelector('label');
-
-    if(hideItem.checked) {
-        label.textContent = 'Unhide notes';
-        ul.style.display = 'none';
-    } else {
-        ul.style.display = 'block';
-        label.textContent = 'Hide notes';
-    }
-});
-
-// ********* search filter
-let searchInput = document.querySelector('#search-note input');
-
-searchInput.addEventListener('keyup', function(e) {
-    let searchChar = e.target.value.toUpperCase();
-
-    let notes = ul.getElementsByTagName('li');
-
-    Array.from(notes).forEach(function(note) {
-        let parText = note.firstElementChild.textContent;
-
-        if(parText.toUpperCase().indexOf(searchChar) !== -1) {
-            note.style.display = 'block';
-        } else {
-            note.style.display = 'none';
-        }
-    })
-});
+let strObj = new String('hello');
+console.log(strObj);
+console.log(typeof strObj);
