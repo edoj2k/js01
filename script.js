a2c27c63a76c0acaecb3959c415ecc3803484aaf
edoj2k <edwardferdian@yahoo.com>
2024-08-23 15:44:35 +0700

    69-call apply bind

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// function calc(num1) {
-//     let num2  = 10;
-    
-//     return function(num3) {
-//         let sum = num1 + num2 + num3;
-//         console.log(sum);
-//     }
-// }
-
-// let add = calc(5);
-
-// console.log(add);
-// add(15);
-
-function a() {
-    let arr = [];
-    for(let i = 0; i < 3; i++) {
-        arr.push((function() {
-            console.log(i);
-        })());
+let person = {
+    firstname: 'John',
+    lastname: 'Smith',
+    fullname: function() {
+        return this.firstname + ' ' + this.lastname;
     }
-    return arr;
-}
+};
 
-a();
+let nick = {
+    firstname: 'Nick',
+    lastname: 'Doe'
+};
+
+console.log(person.fullname.call(nick));
+
+let definePerson = function(age, job) {
+    console.log(this.fullname() + ' is ' + age + ' years old and he is a ' + job);
+};
+
+definePerson.call(person, 28, 'developer');
+definePerson.apply(person, [30, 'designer']);
+
+let getPerson = definePerson.bind(person, 19);
+getPerson('student');
+getPerson('driver');
