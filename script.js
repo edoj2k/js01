b4ab176d0546cc16ed0ea4ab5c206365b01d6ca5
edoj2k <edwardferdian@yahoo.com>
2024-08-29 17:19:58 +0700

    browser local storage

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
+/*************************
+ **** QUIZ CONTROLLER ****
+ *************************/
 let quizController = (function() {
-    let private = 10;
 
-    let privateFn = function(a) {
-        return a + private;
-    }
-
-    return {
-        publicMethod: function() {
-            return privateFn(20);
-        }
-    }
+    //* Question Constructor */
 })();
 
 
+/*************************
+ **** UI CONTROLLER ****
+ *************************/
 let UIController = (function() {
-    let num1 = 30;
 
-    return {
-        sum: function(num2) {
-            return num1 + num2;
-        }
-    }
 })();
 
+/*************************
+ **** CONTROLLER ****
+ *************************/
 let controller = (function(quizCtrl, UICtrl) {
-    console.log(UICtrl.sum(100) + quizController.publicMethod())
 })(quizController, UIController);
