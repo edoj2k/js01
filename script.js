0573ae497ef25b49dfffc5f50dd463b3d0bcde90
edoj2k <edwardferdian@yahoo.com>
2024-09-03 16:48:55 +0700

    107-arrow functions part1

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-// const name = 'John';
-
-// let age = 28;
-
-// let profession = 'Instructor';
-
-// function personES5() {
-//     console.log('My name is ' + name + ', I am ' + age + ' years old and I am ' + profession);
+// const multiply = function(x, y) {
+//     let z = x * y;
+//     return z;
 // }
 
-// personES5();
+const multiply = () => ({firstname: 'John'});
 
-// function personES6() {
-//     return `My name is ${name}, I'm ${age} years old and I'm ${profession}`;
-// }
-
-// personES6();
-
-// function experience(company1, company2) {
-//     console.log(`${personES6()}. I have ${company1 + company2} years of experience in programming`);
-// }
-
-// experience(4, 7);
-
-let greet = 'Hello my dear student';
-
-console.log(greet.startsWith('Hello'));
-console.log(greet.endsWith('ent'));
-console.log(greet.includes('dear'));
-console.log(greet.repeat(3));
+console.log(multiply());
