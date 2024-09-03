4364da45bb0a0d83115b45d250a13f98344dfb5e
edoj2k <edwardferdian@yahoo.com>
2024-09-03 22:03:41 +0700

    111-spread operators

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function person(firstname = 'John', lastname = 'Smith', profession = 'Programmer') {
+// const numbers1 = [1, 2, 3, 4];
 
-    console.log(`I am ${firstname} ${lastname} and I am ${profession}`);
+// const numbers2 = [5, 6, 7, 8];
+
+// let numbers3 = [];
+
+// numbers3 = numbers3.concat(numbers1);
+
+// numbers3.push(10);
+
+// numbers3 = numbers3.concat(numbers2);
+
+// const numbers4 = [10, ...numbers1, 20, ...numbers2, 30];
+
+// console.log(numbers3);
+// console.log(numbers4);
+
+// let names = ['Jane', 'Bob', 'Alex', 'Ann'];
+
+// let namesCopy = [...names];
+
+// names.push('John');
+
+// console.log(names);
+
+// console.log(namesCopy);
+
+function sum(x, y, z) {
+    console.log(x + y + z);
 }
 
-person('Nick', 'Doe', 'Instructor');
+let numbers = [10, 20, 30];
+
+// sum.apply(null, numbers);
+sum(...numbers);
