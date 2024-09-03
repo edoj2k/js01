57fb3fcdd26feae3f028afa2f6e7681013576b21
edoj2k <edwardferdian@yahoo.com>
2024-09-03 17:11:00 +0700

    108-arrow function part2

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-// const multiply = function(x, y) {
-//     let z = x * y;
-//     return z;
+// const numbers = [2, 4, 6, 8];
+
+// let double = numbers.map(number => number * 2);
+
+// console.log(double);
+
+// var personES5 = {
+//     firstname: 'Nick',
+//     lastname: 'Doe',
+//     sayFullName: function() {
+//         return function() {
+//             console.log(this.firstname + ' ' + this.lastname);
+//         }.bind(this);
+//      }
 // }
 
-const multiply = () => ({firstname: 'John'});
+// personES5.sayFullName()();
 
-console.log(multiply());
+var personES6 = {
+    firstname: 'Nick',
+    lastname: 'Doe',
+    sayFullName: function() {
+        return () => {
+            console.log(`${this.firstname} ${this.lastname}`);
+        };
+     }
+}
+
+personES6.sayFullName()();
+
+document.querySelector('h1').addEventListener('click', function() {
+    console.log(this);
+
+    let a = () => {
+        this.style.color = 'red';
+    }
+    a();
+});
